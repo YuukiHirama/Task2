96636d68118e47ce4ff2a0fa05b5b40122116687
YuukiHirama <57606420+YuukiHirama@users.noreply.github.com>
2019-11-13 16:45:50 +0900

    リポジトリを作成

---
+++ Task2/Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by 平間雄樹 on 2019/11/13.
+//  Copyright © 2019 hirama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
