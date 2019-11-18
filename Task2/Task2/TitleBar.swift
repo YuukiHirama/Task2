d7abbe5598cc645a5e5ed8c95689c421b34a6ebb
Yuuki Hirama <y.hirama@appdevteam.work>
2019-11-18 14:01:35 +0900

    valをletへ修正しUIStackViewを使わないように修正

--- Task2/Task2/TitleBar.swift
+++ Task2/Task2/TitleBar.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class CustomView: UIView {
 
-    // MARK: - Properties -
     /// タイトルラベル
     private let titleLabel:UILabel = UILabel()
     /// ログインボタン
@@ -19,37 +18,34 @@ class CustomView: UIView {
     private let logOutButton:UIButton = UIButton(type: UIButton.ButtonType.system)
     /// 各種設定ボタン
     private let settingButton:UIButton = UIButton(type: UIButton.ButtonType.system)
-    /// 共通設定
-    private let commonView = UIStackView(frame: .zero)
-
-    // MARK: - Life cycle events -
-    /// 指定イニシャライザ
-    required override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.commonInit()
-
-    }
-    /// 必須イニシャライザ
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder:aDecoder)!
-        self.commonInit()
-    }
-
+    
     /// 共通初期化処理
-    private func commonInit() {
+    public func commonInit() {
         
-        createTitleLabel()
-        createlLogInButton()
-        createLogOutButton()
-        createSettingButton()
+        self.isUserInteractionEnabled = true
         
-        self.addSubview(commonView)
+        /// タイトルラベル初期化
+        titleLabel.text = "前回ログイン日時2019/11/0712:00:00"
+        titleLabel.font = UIFont.systemFont(ofSize: 16)
+        titleLabel.textColor = UIColor.black
+        /// ログインボタンを作成
+        logInButton.setTitle("ログイン",for: .normal)
+        logInButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        logInButton.addTarget(self, action: #selector(pushLogIn), for: .touchUpInside)
+        /// ログアウトボタンを作成
+        logOutButton.setTitle("ログアウト",for: .normal)
+        logOutButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        logOutButton.addTarget(self, action: #selector(pushLogOut), for: .touchUpInside)
+        /// 各種設定ボタンを作成
+        settingButton.setTitle("各種設定", for: .normal)
+        settingButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+                
         self.addSubview(titleLabel)
         self.addSubview(logInButton)
         self.addSubview(logOutButton)
         self.addSubview(settingButton)
         
-        self.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
+        // ボタン初期表示
         logInButton.isHidden = false
         logOutButton.isHidden = true
     }
@@ -57,75 +53,24 @@ class CustomView: UIView {
     /// 格アイテムのレイアウト調整
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.layoutStackView()
-        self.layoutTitleLabel()
-        self.layoutLoginButton()
-        self.layoutLogOutButton()
-        self.layoutSettingButton()
-    }
 
-    // MARK: - Create subviews -
-    /// タイトルラベルを作成
-    private func createTitleLabel(){
-        titleLabel.text = "前回ログイン日時2019/11/0712:00:00"
-        titleLabel.font = UIFont.systemFont(ofSize: 16)
-        titleLabel.textColor = UIColor.black
-    }
-    /// ログインボタンを作成
-    private func createlLogInButton(){
-        logInButton.setTitle("ログイン",for: .normal)
-        logInButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        logInButton.addTarget(self, action: #selector(pushLogIn), for: .touchUpInside)
-    }
-    /// ログアウトボタンを作成
-    private func createLogOutButton(){
-        logOutButton.setTitle("ログアウト",for: .normal)
-        logOutButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        logOutButton.addTarget(self, action: #selector(pushLogOut), for: .touchUpInside)
-    }
-    /// 各種設定ボタンを作成
-    private func createSettingButton(){
-        settingButton.setTitle("各種設定", for: .normal)
-        settingButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-    }
-
-    // MARK: - Layout subviews -
-    /// 共通設定
-    private func layoutStackView() {
-        commonView.axis = .horizontal
-        commonView.alignment = .center
-        commonView.spacing = 10
-        commonView.translatesAutoresizingMaskIntoConstraints = false
-        commonView.center.x += 10
-        commonView.frame.origin.y += 5
-    }
-    /// タイトルのレイアウト調整
-    private func layoutTitleLabel() {
-        commonView.addArrangedSubview(titleLabel)
-    }
-    /// ログインボタンのレイアウト調整
-    private func layoutLoginButton() {
-        commonView.addArrangedSubview(logInButton)
-    }
-    /// ログアウトのレイアウト調整
-    private func layoutLogOutButton() {
-        commonView.addArrangedSubview(logOutButton)
-    }
-    /// 各種設定のレイアウト調整
-    private func layoutSettingButton() {
-        commonView.addArrangedSubview(settingButton)
+        titleLabel.frame = CGRect(x: 10, y: 30, width: 300, height: 50)
+        logInButton.frame = CGRect(x: 260, y: 30, width: 100, height: 50)
+        logOutButton.frame = CGRect(x: 270, y: 30, width: 100, height: 50)
+        settingButton.frame = CGRect(x: 330, y: 30, width: 100, height: 50)
     }
     
-    // MARK: - button events -
+    // MARK: - EVENT
     /// ログインボタンが押された
     @objc func pushLogIn(sender: UIButton){
         logInButton.isHidden = true
         logOutButton.isHidden = false
+        settingButton.frame = CGRect(x: 340, y: 30, width: 100, height: 50)
     }
     /// ログアウトボタンが押された
     @objc func pushLogOut(sender: UIButton){
         logInButton.isHidden = false
         logOutButton.isHidden = true
+        settingButton.frame = CGRect(x: 330, y: 30, width: 100, height: 50)
     }
-
 }
--- Task2/Task2/ViewController.swift
+++ Task2/Task2/ViewController.swift
@@ -10,34 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Properties -
     /// カスタムビュー
-    private let customView:CustomView = CustomView()
-
-    // MARK: - Life cycle events -
-    // ビューの初期ロード
+    private let customView = CustomView()
+    
+    /// ビューの初期ロード
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(customView)
+        view.addSubview(customView)
+        customView.commonInit()
     }
     
-    // レイアウト設定
+    /// レイアウト設定
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.layoutCustomView()
+        customView.frame = self.view.frame
     }
-
-    // MARK: - Layout subviews -
-    /// カスタムビューのサイズと位置指定
-    private func layoutCustomView() {
-        // iOS11よりも上だったら
-        if #available(iOS 11.0, *) {
-            customView.frame.origin = self.view.safeAreaLayoutGuide.layoutFrame.origin
-            customView.frame.size = CGSize(width: self.view.safeAreaLayoutGuide.layoutFrame.width, height: 44)
-        }else{
-        // iOS11よりも下だったら
-            customView.frame.origin = self.view.frame.origin
-            customView.frame.size = CGSize(width: self.view.frame.width, height: 44)
-        }
-    }
+        
 }
